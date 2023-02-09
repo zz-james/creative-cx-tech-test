97f67a3ac7553ccbd57fc4dd5168b80f2ea2d86c
zz-james <zz.james@gmail.com>
2023-02-09 15:35:53 +0000

    feat: add two variations imdb link and styled titles

--- src/component/App.js
+++ src/component/App.js
@@ -6,19 +6,17 @@ import Header from "./Header";
 import MovieDetails from "./MovieDetails";
 import MoviesList from "./MoviesList";
 
-
 const App = () => {
-
-    return (
+  return (
     <div className="App">
       <Header text="MDB" />
       <p className="App-intro">Sharing a few of our favourite movies</p>
       <Switch>
-          <Route exact path="/">
-            <MoviesList />
-          </Route>
-          <Route path="/details/:id" component={MovieDetails} />
-          </Switch>
+        <Route exact path="/">
+          <MoviesList />
+        </Route>
+        <Route path="/details/:id" component={MovieDetails} />
+      </Switch>
     </div>
   );
 };
--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,30 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { useDecision } from "@optimizely/react-sdk";
+
 const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
-
 const Movie = ({ movie, movies }) => {
+  const [styledTitleDecision] = useDecision(
+    "styled_titles",
+    { autoUpdate: true },
+    {
+      /* (Optional) User overrides */
+    }
+  );
+
   const poster =
     movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
   return (
-    <Link to={{pathname: `details/${movie.imdbID}`, query: { id: movie.imdbID, movies: movies }}}>
-        <div className="movie">
-      <h2>{movie.Title}</h2>
-      <div>
-        <img
-          width="200"
-          alt={`The movie titled: ${movie.Title}`}
-          src={poster}
-        />
+    <Link
+      className={styledTitleDecision.enabled ? "movieTitle" : undefined}
+      to={{
+        pathname: `details/${movie.imdbID}`,
+        query: { id: movie.imdbID, movies: movies },
+      }}
+    >
+      <div className="movie">
+        <h2>{movie.Title}</h2>
+        <div>
+          <img
+            width="200"
+            alt={`The movie titled: ${movie.Title}`}
+            src={poster}
+          />
+        </div>
+        <p>({movie.Year})</p>
       </div>
-      <p>({movie.Year})</p>
-    </div>
     </Link>
-
   );
 };
-
 
 export default Movie;
